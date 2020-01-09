1e4602395190829e066f3d61e2f267443336f034
s14228so <saongtx7@gmail.com>
2020-01-09 15:11:44 +0900

    searchアイコンの修正

--- todo-web/components/TodoList.vue
+++ todo-web/components/TodoList.vue
@@ -3,7 +3,7 @@
     <v-card-title>
       Todo List
       <v-spacer></v-spacer>
-      <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+      <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
     <v-data-table :headers="headers" :items="todos" :search="search">
       <template v-slot:item.action="{ item }">
--- todo-web/plugins/auth-check.js
+++ todo-web/plugins/auth-check.js
@@ -4,11 +4,11 @@ import axios from "@/plugins/axios"
 const authCheck = ({ store, redirect }) => {
     firebase.auth().onAuthStateChanged(async user => {
         if (user) {
-            console.log("ユーザーイ流よ")
+            console.log("ユーザーいるよ")
             const { data } = await axios.get(`/v1/users?uid=${user.uid}`)
             store.commit("setUser", data)
         } else {
-            console.log("ユーザーイナヨ")
+            console.log("ユーザーいないよ")
             store.commit("setUser", null)
         }
     });
